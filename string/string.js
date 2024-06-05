820bf7d3744a6f51a30585082be462bd982c701c
Ebad ur rehman <139793411+ebad-ur@users.noreply.github.com>
2024-06-05 22:51:54 +0500

    Add files via upload
    String and string concatenation

---
+++ string/string.js
@@ -0,0 +1,59 @@
+
+
+// declare the variable 
+let firstName = "ebad ";
+
+// print the value to find the index it also coubnt the spaces 
+console.log(firstName[4]);
+
+// to find the length of string 
+
+// declare the variable
+
+let first_names ="ebad ur rehman ";
+//print the value 
+console.log(first_names.length);
+
+// find the alphabet from the index it 
+console.log(first_names[first_names.length-2]);
+
+
+//String concattentail
+//it is used to add to string 
+
+//intilize two variable 
+
+let string1="ebad"
+let string2="ur rehman"
+
+// adding both of them and putting in new variable 
+let lastname = string1 + " " + string2
+
+//printnig the value 
+console.log(lastname);
+
+// same method for adding two number 
+
+//intilize two variable 
+let string3 = "17"
+let string4 = "10"
+
+// adding both of them and putting in new variable  and change the value in number 
+let number =  +string3 +  +string4
+
+//printnig the value 
+console.log(number)
+
+//templarte string use to  write the string in opne frame 
+
+//intilize two variable 
+let age = 23
+let names = "ebad ur rehman"
+
+
+// wrtting in new varaible 
+let aboutme = `My name is ${names} and my age is ${age}`
+
+
+//printnig the value 
+console.log(aboutme)
