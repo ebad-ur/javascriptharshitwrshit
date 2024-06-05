35c92cf2f7c0ce8bf6fbb7f18139a99f0ac9818a
Ebad ur rehman <139793411+ebad-ur@users.noreply.github.com>
2024-06-05 22:40:08 +0500

    Add files via upload
    Use full method in string

---
+++ methodstring/methodstring.js
@@ -0,0 +1,54 @@
+
+// this is trim method which is use to remove the spaces from the program  
+// declare variable
+let first_names = "           ebad          "
+
+// find the length of program 
+console.log(first_names.length);
+
+//use trim method for removing spaces 
+first_names = first_names.trim();
+
+// print after applying method 
+console.log(first_names);
+
+// checvking length again 
+console.log(first_names.length);
+
+// this is the upper case which is use to convert all the letter in captial 
+
+// intiliaze  the variable 
+let firstName = "ebad ur rehman"
+
+// printing the value 
+console.log(firstName)
+
+// using the touppercasemethod() 
+firstName = firstName.toUpperCase();
+
+//printing the value again 
+console.log(firstName)
+
+// intilize the value 
+let firstebad = "EBAD";
+
+// print the vlaue 
+console.log(firstebad);
+
+// applying the tolwercase method()
+firstebad = firstebad.toLowerCase();
+
+// printing value again
+console.log(firstebad);
+
+//using slice method()
+let firstnames$ = "ebad ur rehman"
+
+//printing the value 
+console.log(firstnames$);
+
+
+//using slice() method for this we make new variable 
+
+let newString = firstnames$.slice(6,8);
+console.log(newString);
