a2b892bff0bdbb3c81839bc77e858a72767c8008
Ebad ur rehman <139793411+ebad-ur@users.noreply.github.com>
2024-06-05 22:58:21 +0500

    Add files via upload
    Truthy-falsevalues with condition

---
+++ truthyfalsy/truthyandfalsy.js
@@ -0,0 +1,41 @@
+//itiliaze the value with falsy value 
+//0
+// ""
+//null
+//undefined
+
+// itliaze the value
+let firstname =0;
+// applying condition 
+if(firstname){
+    console.log(firstname);
+}
+else{
+    console.log("first name val;ue is flasy ");
+}
+
+//trythy value 
+//"sfsdf"
+//1,
+
+//itliaze the value 
+let firstName ="ebad";
+
+//applyinf the condition 
+if(firstName){
+    console.log(firstName);
+}
+else{
+    console.log("this is flasy ")
+}
+
+//turnery opearor 
+
+//itliaze the value 
+let age =3;
+
+//using turnery operator 
+let drink = age >=5 ? "coffee" : "milk"
+
+//printting the value 
+console.log(drink);
