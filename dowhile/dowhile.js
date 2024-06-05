54d2534e2b64a40f6cc71ece88c48d2b1adc026d
Ebad ur rehman <139793411+ebad-ur@users.noreply.github.com>
2024-06-05 23:13:39 +0500

    Add files via upload
    Do while loop

---
+++ dowhile/dowhile.js
@@ -0,0 +1,18 @@
+
+// itt is a loop which will be excute once weather the situation is true or not 
+//it will implement once 
+
+let i=0;
+//do loop do is ised what we have to implment 
+do {
+    console.log(i);
+    i++;
+}while(i<=20)
+
+
+let j=21;
+//do loop do is ised what we have to implment 
+do {
+    console.log(j);
+    j++;
+}while(j<=20)
