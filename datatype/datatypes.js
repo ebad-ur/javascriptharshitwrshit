72d4ec685ed1301d8a2e2a29702d150e948ae179
Ebad ur rehman <139793411+ebad-ur@users.noreply.github.com>
2024-06-05 22:47:06 +0500

    Add files via upload
    Data types and type of data conservation

---
+++ datatype/datatypes.js
@@ -0,0 +1,53 @@
+
+// declaring variable 
+
+let age = 22;
+let names = "ebad";
+
+// checking data type 
+console.log(typeof names);
+
+//convert number in string
+let ages = 33;
+
+//printing the type 
+console.log(typeof ages);
+
+//now converting ibn the string by addding empty string ""
+ages = ages + ""
+
+//printting to see that data type is changed or not
+console.log(typeof (ages));
+
+//convert string into number 
+
+//intiliaze the variable 
+let myStr = "33"
+
+//checking the data type
+console.log(typeof myStr);
+
+// now converting the value of the string in to number  adding the number before the string
+myStr = +"33"
+
+//printing the value again and check the data type
+console.log(typeof (myStr));
+
+//undefined
+
+//when we declare the variable and dosent assign  the value 
+
+//intiliaze value 
+let first_names;
+
+//printting the value 
+console.log(typeof first_names)
+
+//NULL it is a null value 
+let myvariable =null;
+console.log(myvariable);
+
+//Bigint 
+let my = BigInt(1321654684146576835498735413248);
+console.log(my);
+
