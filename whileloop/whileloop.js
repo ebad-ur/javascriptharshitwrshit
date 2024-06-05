244db11bc0ae9897b03d6c4f902c2a1206ce974d
Ebad ur rehman <139793411+ebad-ur@users.noreply.github.com>
2024-06-05 23:09:58 +0500

    Add files via upload
    While loop

---
+++ whileloop/whileloop.js
@@ -0,0 +1,38 @@
+
+// it is used to run the condtion for multiple times it will run until the value of condtion is true 
+//when condtion become false the value will come out side the loop and it will not excute further
+
+// intliaze the value
+
+let i=0;
+
+//applying while loop
+while (i < 12){
+    console.log(i);
+    i++;
+}
+
+console.log(`loop is comming outside from the program when the value become  ${i}`)
+
+// sum of 100 number
+
+//intliaze the variable 
+
+let num = 100;
+let total =0;
+let j = 0;
+//applying while loop
+while (j<=100){
+    total = total+j
+    j++;
+    
+}
+console.log(total)
+console.log(`loop is comming outside from the program when the value become  ${j}`)
+
+
+
+
+//it is fast then while loop and it is a math formula 
+// let total =(num*(num+1))/2;
+// console.log(total)
