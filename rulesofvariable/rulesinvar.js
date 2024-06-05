de278dd0fab749e9f9456ef57f5ed1819791c9e6
Ebad ur rehman <139793411+ebad-ur@users.noreply.github.com>
2024-06-05 22:25:58 +0500

    Add files via upload
    Rules for variable

---
+++ rulesofvariable/rulesinvar.js
@@ -0,0 +1,19 @@
+
+
+// you cannot start value with number but you can end the value with the number 
+var value1 = "2"
+console.log(value1);
+
+// you can use only under score and dollar sign in specail symbol you can use in front end and in  middle
+
+var firstnames$="Ebad"
+console.log(firstnames$);
+
+// you can not use space  
+
+var first_names = "Shahzaib"
+console.log(first_names);
+
+// COnvention 
+//start with small letter 
+
