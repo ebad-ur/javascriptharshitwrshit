feb9a80cea78f9551954a2e4661404ca83371848
Ebad ur rehman <139793411+ebad-ur@users.noreply.github.com>
2024-06-05 22:25:07 +0500

    Add files via upload
    Introduction to variable

---
+++ variable/variable.js
@@ -0,0 +1,21 @@
+"use strict";
+
+
+
+
+// declare variable 
+var firstName = "Ebad ur rehmnan";
+
+// use a variabe
+
+console.log(firstName);
+
+// Change value 
+
+firstName = "Shariq"
+
+// use a change value
+
+console.log(firstName);
+
+
